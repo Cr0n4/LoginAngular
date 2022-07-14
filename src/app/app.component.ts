8b9465c4b9b6decd094c65e96dc80b8f6f5f00de
cr0n4 <jon.cl371@gmail.com>
2022-07-14 01:16:11 -0400

    montando firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'loginAngularFirebase';
+
+  usuario = {
+    email: '',
+    password: ''
+  }
+
+  constructor(private authService: AuthService){
+
+  }
+
+  Ingresar(){
+    console.log(this.usuario);
+    const { email, password } = this.usuario;
+    this.authService.login(email, password).then(res => {
+      console.log("Se registro: ", res);
+    });
+  }
+
+  IngresarConGoogle(){
+    const { email, password } = this.usuario;
+    this.authService.loginWithGoogle(email, password).then(res => {
+      console.log("Se registro: ", res);
+    });
+  }
+  obtenerUsuarioLogeado(){
+    this.authService.getUserLogged().subscribe(res =>{
+      console.log(res?.email);
+    });
+  }
+
+  logout(){
+    this.authService.logout();
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+import {AngularFireModule} from '@angular/fire/compat';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDod0OzvyytYvUl2ZSEsa6bf4uxxCpEV4g",
+  authDomain: "loginnice.firebaseapp.com",
+  projectId: "loginnice",
+  storageBucket: "loginnice.appspot.com",
+  messagingSenderId: "200969953059",
+  appId: "1:200969953059:web:3e435fdd1faad006147696",
+  measurementId: "G-GYGT4H9TTG"
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    FormsModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
